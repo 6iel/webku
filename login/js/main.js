01c3c5f5797d005a41d090838b71b5e0507f3ca8
6iel <bilqisaulia2610@gmail.com>
2026-05-19 21:04:42 +0700

    Update main.js

--- login/js/main.js
+++ login/js/main.js
@@ -18,3 +18,36 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+document
+.getElementById("loginForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(username === "heri" &&
+   password === "123"){
+
+localStorage.setItem(
+"username",
+username
+);
+
+alert("Login berhasil!");
+
+window.location.href =
+"../index.html";
+
+}else{
+
+alert("Username atau password salah!");
+
+}
+
+});
